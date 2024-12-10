b41ec7651c1400328f312171ad810648f59e1d16
Zurd46 <dzurmuehle@gmail.com>
2024-12-10 19:59:56 +0100

    update

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -6,7 +6,6 @@ namespace RaumReservierungsSystem.Data
     public class ApplicationDbContext : DbContext
     {
         public DbSet<Reservation> Reservations { get; set; }
-        public DbSet<Room> Rooms { get; set; }
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
--- Models/Room.cs
+++
@@ -1,9 +0,0 @@
-namespace RaumReservierungsSystem.Models
-{
-    public class Room
-    {
-        public int Id { get; set; } // Primärschlüssel
-        public int RoomNumber { get; set; }
-        public bool IsAvailable { get; set; } = true;
-    }
-}
